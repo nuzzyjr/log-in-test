43923c8deca9592401e489c3ab65654ad4dfe3fc
nuzzyjr <nicolf15@icloud.com>
2022-01-24 10:45:44 +0000

    dd

--- insert.php
+++ insert.php
@@ -2,27 +2,43 @@
    function insert($conn, $user)
    {
        //does email already exist in DB?
-       $sql = "
-       INSERT INTO users(
-           email,
-           password,
-           token,
-           is_active
-           ) VALUES (
-           
-           '{$user->email}',
-           '{$user->password_hash}',
-           '{$user->token}',
-           '0'
-           )
-       ";
 
-       //create mysql query 
-       $sqlQuery = mysqli_query($conn, $sql);
+        $email_search = "SELECT * FROM users WHERE email = '".$user->email."'";
+        $result = mysqli_query($conn, $email_search);
+        
 
-       if (!$sqlQuery)
-       {
-           die ("MySQL query failed!" . mysqli_error($conn));
-       }
+        if ($not_dupe)
+        {
+
+            $sql = "
+            INSERT INTO users(
+                email,
+                password,
+                token,
+                is_active
+                ) VALUES (
+                
+                '{$user->email}',
+                '{$user->password_hash}',
+                '{$user->token}',
+                '0'
+                )
+            ";
+
+            //create mysql query 
+            $sqlQuery = mysqli_query($conn, $sql);
+
+            if (!$sqlQuery)
+            {
+                die ("MySQL query failed!" . mysqli_error($conn));
+            }
+
+        }
+
+        else
+        {
+           echo "Email taken!";
+        }
+       
    }
 ?>
