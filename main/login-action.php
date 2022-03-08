b948fb6bd3b451e88f043199f033605bf7b8875e
nuzzyjr <89519055+nuzzyjr@users.noreply.github.com>
2022-03-08 09:48:10 +0000

    bob

--- main/login-action.php
+++ main/login-action.php
@@ -1,40 +1,48 @@
 <?php
 
 
+include_once "C:/xampp/htdocs/Projects/GibJohn/libraries/login.php";
 include_once "C:/xampp/htdocs/Projects/GibJohn/libraries/db.php";
-include_once "C:/xampp/htdocs/Projects/GibJohn/libraries/user.php";
 
-//create user object
+$email = $_POST["email"];
+$password = $_POST["password"];
 
-$user = new User($conn, $_POST['email'], $_POST['password'], "", "", "");
+$details = list($email,$password);
 
-$user->authenticate();
-
-
-
-//assign user object their name and account_type
+$logged_in = login($email, $password);
 
 
 //check if user wants to be remembered
 if (isset($_POST["remember_me"]) && $_POST["remember_me"] == "Yes")
 {
-    setcookie("remember_me", serialize($user), time() + 60*60*24*7*4*2 ); //two months cookie
+    setcookie("remember_me", serialize($details), time() + 60*60*24*7*4*2 ); //two months cookie
 }
 
-//if login successful
-if ($user->is_logged_in())
+//if login successful as student
+if ($logged_in == "student")
 {
-
+    
     session_start();
-    $_SESSION['user'] = serialize($user);
-    header("Location: index.php");
+    $_SESSION['user'] = serialize($details);
+    header("Location: student_dashboard.php");
     
 }
+
+//if login successful as teacher
+else if ($logged_in == "teacher")
+{
+    
+    session_start();
+    $_SESSION['user'] = serialize($details);
+    header("Location: teacher_dashboard.php");
+    
+}
+
 //if can't log in
 else
 {
     session_start();
-    $err = $user->authenticate();   
+    $err = $->authenticate();   
     $_SESSION['login_err'] = $err;
     header("Location: login.php");
 
