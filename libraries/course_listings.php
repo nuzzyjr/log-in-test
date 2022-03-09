aafda755204324c3d8f41b96b4a101e9132b58d0
nuzzyjr <89519055+nuzzyjr@users.noreply.github.com>
2022-03-09 12:20:39 +0000

    as
    qqq

---
+++ libraries/course_listings.php
@@ -0,0 +1,44 @@
+<?php
+
+    //POPULATE COURSES
+
+    function populate()
+    {
+        
+        include_once "C:/xampp/htdocs/Projects/GibJohn/libraries/db.php";
+        $conn = get_conn();
+        $result = mysqli_query($conn, "
+        
+        SELECT courseName, courseDescription FROM courses
+
+        ");
+
+        $count = 1;
+
+        while ($row = $result->fetch_assoc()) {
+            
+            if ($count / 3 == 0 or $count==1){
+              
+                echo'ouhuiuui';
+            }
+            
+            echo'  
+            
+            <div class="card" style="width:20vw; height:15vw;">
+            <h5 class="card-header">Course</h5>
+            <div class="card-body">
+                <h5 class="card-title">'.$row["courseName"].'</h5>
+                <p class="card-text">'.$row["courseDescription"].'</p>
+                <a href="COURSE LINK" class="btn btn-primary">Join Course</a>
+            </div>
+            </div>
+            
+            ';
+            
+            $count = $count + 1;
+        }
+      
+  
+    }
+
+?>
---
+++ student_dashboard.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Dashboard</title>
+    <link href="stylesheet.css" rel="stylesheet" type="text/css" />
+    <link href="bootstrap-css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    
+    <!--NAV BAR-->
+    <nav class="sticky-top navbar">
+    <img src="images/book.png" style="margin-left:48%; max-width:55px; border-radius:55px; border: 3.5px solid white; box-shadow:none; max-height: 4vw; " onclick="Location: href='index.php'" />
+    <button class="btn btn-primary" onclick="location = href='account.php'" style="float:right;margin-right:10px; box-shadow:none; color:black; max-height: 4vw;" >Account</button>
+    </nav>
+
+    <!--CONTAINER-->
+    <div class="container">
+        <!--SEARCH BAR-->
+        <div class="row" style="margin-top:7vw;">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+            <form class="d-flex">
+            <input class="form-control me-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+            </form>
+            <div class="col-md-2"></div>
+        </div> 
+        </div>
+    </div>
+
+    <!--COURSE LISTINGS-->
+  
+    <?php
+        include_once "C:/xampp/htdocs/Projects/GibJohn/libraries/course_listings.php";
+        populate();
+    ?>
+    
+    <!--FOOTER-->
+    <footer class="footer">
+    
+        </br>Find us at:</br>
+        Instagram: @GibJohn</br>
+        Facebook: @GibJohn</br>
+        Email: gibjohn@gmail.com</br>
+        Phone: 07473820938
+
+    </footer>
+</body>
+</html>
